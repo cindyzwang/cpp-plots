d3ab4a6c6ee5c40d0c94d5242275dde313894fc0
Cindy Wang <cindy.wang@synthego.com>
2021-05-28 14:17:09 -0700

    init build and plot

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cmath>
+#include <matplot/matplot.h>
+
+#include "Plots.h"
+
+int main() {
+  printf("%d.%d\n", Plots_VERSION_MAJOR, Plots_VERSION_MINOR);
+  using namespace matplot;
+  std::vector<double> x = linspace(0, 2 * pi);
+  std::vector<double> y = transform(x, [](auto x) { return sin(x); });
+
+  plot(x, y, "-o");
+  hold(on);
+  plot(x, transform(y, [](auto y) { return -y; }), "--xr");
+  plot(x, transform(x, [](auto x) { return x / pi - 1.; }), "-:gs");
+  plot({1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1}, "k");
+
+  show();
+  return 0;
+}
